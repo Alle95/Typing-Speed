5c161723c96a601e338611e7a867a3cff6d47f19
Alle95 <147329984+Alle95@users.noreply.github.com>
2023-12-05 22:34:05 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,61 @@
+let words = ["car", "ball","against","there","story","could","something",
+"wind","door","star","side","think","vocabulary","score","confused","illusion",
+"smartest","world","dictionary","looking","faster","learning",
+"research","teacher","moment","education","student","country","mother","money",
+"story","fact","month","book","business","issue","side",
+"kind","head","house","service","friend","father","power","member",
+"law","community","body","information","level","office","health",
+"reason","air","girl","boy","kid","women","men","end"];
+let setTimer;
+
+function shuffle() {
+    let currentIndex = words.length,  randomIndex;
+    while (currentIndex > 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [words[currentIndex], words[randomIndex]] = [
+        words[randomIndex], words[currentIndex]];
+  }
+}
+
+function createText() {
+    document.getElementById("startButton").onclick = null;
+    shuffle();
+    let letterId = 0;
+    for (let i = 0; i < words.length; ++i) {
+        let word = words[i];
+        for (let j = 0; j < word.length; ++j) {
+            let letter1 = word[j];
+            let letter = document.createElement('span');
+            ++letterId;
+            letter.id = letterId;
+            letter.innerHTML = letter1;
+            document.getElementById('text').append(letter);
+        }
+        let letter = document.createElement('span');
+        ++letterId;
+        letter.id = letterId;
+        letter.innerHTML = ' ';
+        document.getElementById('text').append(letter);
+    }
+    setTimer = setInterval(changeTime, 1000);
+    document.addEventListener("keyup", apearLetter);
+    
+}
+
+let letterId = 1;
+
+function apearLetter(event) {
+    if (event.key == document.getElementById(letterId).textContent) {
+        document.getElementById(letterId).style="color:greenyellow";
+    } else {
+        document.getElementById(letterId).style="color:red";
+    }
+    ++letterId;
+}
+let time = 60;
+
+function changeTime() {
+--time;
+timer.innerHTML = time;
+}
